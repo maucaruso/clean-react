2dc6461b66554060f31451fe3e1520e9e0fdd551
maucaruso <carusojr@outlook.com>
2022-09-22 07:25:15 -0300

    fix: webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,11 @@ module.exports = {
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
   },
   externals: {
     react: 'React',
